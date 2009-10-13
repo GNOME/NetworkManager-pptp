GNOME/NetworkManager-pptp

commit 22d8b96a294673431a94c4976870cfbd2746d469

Patch from Russell Suter <rsuter@mxlogic.com>
2009-02-06  Dan Williams  <dcbw@redhat.com>

	Patch from Russell Suter <rsuter@mxlogic.com>

	* properties/advanced-dialog.c
		- (advanced_dialog_new_hash_from_dialog): fix saving MPPE values


svn path=/trunk/; revision=21

properties/advanced-dialog.c
@@ -385,9 +385,10 @@ advanced_dialog_new_hash_from_dialog (GtkWidget *dialog, GError **error)
 			g_hash_table_insert (hash, g_strdup (NM_PPTP_KEY_REQUIRE_MPPE_40), g_strdup ("yes"));
 			break;
 		default:
-			g_hash_table_insert (hash, g_strdup (NM_PPTP_KEY_REQUIRE_MPPE), g_strdup ("yes"));
 			break;
 		}
+
+		g_hash_table_insert (hash, g_strdup (NM_PPTP_KEY_REQUIRE_MPPE), g_strdup ("yes"));
 
 		widget = glade_xml_get_widget (xml, "ppp_allow_stateful_mppe");
 		if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (widget)))

commit 0c39c37fe4a395529bb7025027f60931152fd6ac

properties: really fix saving MPPE settings (lp:371402)
Found by David Jordan and others

Fixed for some, but broken for others by commit
22d8b96a294673431a94c4976870cfbd2746d469.  REQUIRE_MPPE will pass
--require-mppe to pppd, which will let pppd accept both 128 and 64
bit connections.  Some servers have policy that will reject clients
that accept lower-security encryption, so setting REQUIRE_MPPE
only when neither 128 or 64 are set is the right fix.

properties/advanced-dialog.c
@@ -385,10 +385,9 @@ advanced_dialog_new_hash_from_dialog (GtkWidget *dialog, GError **error)
 			g_hash_table_insert (hash, g_strdup (NM_PPTP_KEY_REQUIRE_MPPE_40), g_strdup ("yes"));
 			break;
 		default:
+			g_hash_table_insert (hash, g_strdup (NM_PPTP_KEY_REQUIRE_MPPE), g_strdup ("yes"));
 			break;
 		}
-
-		g_hash_table_insert (hash, g_strdup (NM_PPTP_KEY_REQUIRE_MPPE), g_strdup ("yes"));
 
 		widget = glade_xml_get_widget (xml, "ppp_allow_stateful_mppe");
 		if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (widget)))