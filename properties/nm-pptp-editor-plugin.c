b612a5555cbf3948e84470f48ef3a7537f2800e1
Thomas Haller <thaller@redhat.com>
2016-06-16 12:42:53 +0200

    shared: reimport shared files and reorganize
    The shared files got reorganized and the directory
    "shared/nm-vpn" got merged with "shared/nm-utils".
    Update the VPN plugin to do the same.
    
    Also, reimport and update the shared files with their
    latest version, fix the include paths and reword
    "shared/README".

--- properties/nm-pptp-editor-plugin.c
+++ properties/nm-pptp-editor-plugin.c
@@ -28,7 +28,7 @@
 #ifdef NM_VPN_OLD
 #include "nm-pptp-editor.h"
 #else
-#include "nm-vpn/nm-vpn-plugin-utils.h"
+#include "nm-utils/nm-vpn-plugin-utils.h"
 #endif
 
 #define PPTP_PLUGIN_NAME    _("Point-to-Point Tunneling Protocol (PPTP)")
